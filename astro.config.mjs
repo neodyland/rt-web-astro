38026c60682ad17ad3eeb5e0dc6c1a75224f3495
maamokun/MikanDev <maamo-kun@outlook.com>
2024-08-18 01:58:07 +0900

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), react(), starlight({
+    title: 'RT Documentation',
+  })]
+});
