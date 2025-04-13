e666835747bfd50ae26d5d4cf401a298e2c6c7fa
maamokun/MikanDev <maamo-kun@outlook.com>
2025-04-13 20:37:56 +0900

    chore: Update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://rt.neody.land/",
+    vite: {
+        plugins: [tailwindcss()],
+    },
     integrations: [
-        tailwind(),
         react(),
         starlight({
             title: "RT Documentation",
